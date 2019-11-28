e4212f0228b1573ed435706ad5af7025b2a087ce
Zan <41676687+noolep@users.noreply.github.com>
2019-11-27 17:05:32 -1200

    Update shell_check.sh

--- shell_check.sh
+++ shell_check.sh
@@ -14,7 +14,7 @@ white='\e[1;37m'
 UserAgent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
 
 # dependencies
-dependencies=( "curl")
+dependencies=( "curl" "dnsutils" )
 for i in "${dependencies[@]}"
 do
     command -v $i >/dev/null 2>&1 || {
