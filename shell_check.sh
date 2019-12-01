be0928606f57e74d24992df6ad101fcea1ab3a71
Zan <41676687+noolep@users.noreply.github.com>
2019-11-30 13:57:39 -1200

    Update shell_check.sh

--- shell_check.sh
+++ shell_check.sh
@@ -13,6 +13,16 @@ white='\e[1;37m'
 # User agent
 UserAgent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
 
+# dependencies
+dependencies=( "curl")
+for i in "${dependencies[@]}"
+do
+    command -v $i >/dev/null 2>&1 || {
+        echo >&2 "$i : Not installed!"
+        exit
+    }
+done
+
 # start
 # banner
 echo -e '''
@@ -33,19 +43,19 @@ function JavaGhostShellChk(){
 		check_os=$(curl --user-agent "${UserAgent}" -s "$url" --compressed | grep -o "Linux\|Ubuntu\|Windows\|CentOS")
 		if [[ $check_os =~ "Linux" ]] || [[ $check_os =~ "Ubuntu" ]] || [[ $check_os =~ "Windows" ]] || [[ $check_os =~ "CentOS" ]]; then
 			local shell_name=$(curl --user-agent "${UserAgent}" -s $url --compressed | grep -o "<title>.*" | cut -d ">" -f2 | cut -d "<" -f1)
-			local shell_system=$(curl --user-agent "${UserAgent}" -s $url --compressed | grep -o "Linux.*" | cut -d "<" -f1)
+			local shell_system=$(curl --user-agent "${UserAgent}" -s $url --compressed | grep -o "Linux.*\|Ubuntu.*\|Windows.*\|CentOS.*" | cut -d "<" -f1)
 			local shell_Ips=$(dig +short $(echo $url | sed 's|https://www.||g;s|https://||g;s|http://||g' | cut -d "/" -f1,2,3 | sed "s|http://||g"))
-			printf "$white[ $green%sLIVE%s$white ] $red-$white $url $red[$white Shell name $red:$green $shell_name $red]\n$white[$red+$white] Sys $red:$green $shell_system\n$white[$red+$white] IPs $red:$green $shell_Ips\n"
+			printf "$white[ $green%sLIVE%s$white ] $red-$white $url $red[$white Shell name $red:$green $shell_name $red]\n$white[$red+$white] Sys $red:$yellow $shell_system\n$white[$red+$white] IPs $red:$yellow $shell_Ips\n"
+			echo $url >> live_shell.txt
     	elif [[ $(curl --user-agent "${UserAgent}" -s "$url" --compressed | grep -o '<input type="password"\|<input type=password') =~ "password" ]]; then
     		printf "$white[ $green%sLIVE%s$white ] $red-$white $url $red[$white Shell with password $red-$white cant show info $red]$white"
-    		echo ""
+			echo $url >> live_shell.txt
     	else
     		printf "$white[ $green%sLIVE%s$white ] $red-$white $url\n$white[$red-$white] Information not showing $red[$white contact $red:${blue} https://fb.me/n00b.me $red]$white\n"
-    		echo ""
+			echo $url >> live_shell.txt
     	fi
 	else
 		printf "$white[ $red%sDEAD%s$white ] $red-$white $url\n"
-		echo ""
 	fi
 }
 
@@ -58,5 +68,5 @@ function JavaGhostShellChk(){
 	wait
 )
 
-printf "$white[$red+$white] Total live shell $red:$green  "$( < live_shell.txt wc -l)"\n"
+printf "\n\n$white[$red+$white] Total live shell $red:$green  "$( < live_shell.txt wc -l)"\n"
 # end
